9e1511a9a1381367cafd369fcd883c347b1902e8
Jean-Christophe SAAD-DUPUY <jc.saaddupuy@fsfe.org>
2015-03-21 19:10:03 +0100

    loop i a thread

--- tcs/tcs.py
+++ tcs/tcs.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import threading
 
 import libtorrent as lt
 
@@ -16,6 +17,7 @@ class TcError(Exception):
 
 class TcDeadProxyError(TcError):
     pass
+
 
 
 class TorrentClient(object):
@@ -38,7 +40,7 @@ class TorrentClient(object):
         self.session.set_alert_mask(
                 lt.alert.category_t.storage_notification +
                 lt.alert.category_t.status_notification +
-                lt.alert.category_t.all_categories
+                lt.alert.category_t.progress_notification
         )
 
         self.proxy_type = proxy_type
@@ -51,6 +53,9 @@ class TorrentClient(object):
         self.configure(anonymous_mode)
 
         self.alert_handlers = {}
+
+        self._loop_thread = threading.Thread(target=self._loop)
+        self.e_stop = threading.Event()
 
     def configure(self, anonymous_mode):
         settings = lt.session_settings()
@@ -116,14 +121,12 @@ class TorrentClient(object):
     def add_torrent(self, torrent_path, download_path=None, alert_handler=None):
         torrent_path = os.path.abspath(torrent_path)
         handler = self._add_torrent(torrent_path, download_path)
-        handler.pause()
 
         self.alert_handlers[handler.name()] = alert_handler
 
     def add_magnet(self, magnet, download_path=None, alert_handler=None):
         handler = self._add_magnet(magnet, download_path)
-        handler.pause()
-        self.alert_handlers[handler] = alert_handler
+        self.alert_handlers[handler.name()] = alert_handler
 
     def add(self, what, download_path=None, alert_handler=None):
         if what.startswith("magnet:"):
@@ -131,10 +134,15 @@ class TorrentClient(object):
         elif os.path.isfile(what):
             return self.add_torrent(what, download_path, alert_handler)
         else:
-            raise ValueError()
+            raise ValueError("%s not usable", what)
 
-    def loop(self):
-        while(True):
+    def stop(self):
+        self.e_stop.set()
+        if self._loop_thread.start != threading.currentThread():
+            self._loop_thread.join()
+
+    def _loop(self):
+        while not self.e_stop.isSet():
             self.session.wait_for_alert(500)
             alert = self.session.pop_alert()
             if not alert:
@@ -142,7 +150,7 @@ class TorrentClient(object):
 
             handle = getattr(alert, "handle", None)
             if not handle:
-                logger.debug("No handle for %s", alert)
+                logger.debug("No handle for %s", alert.__class__.__name__)
                 continue
             alert_handler = self.alert_handlers.get(alert.handle.name(), None)
             if not alert_handler:
@@ -150,9 +158,15 @@ class TorrentClient(object):
             m_name = "on_%s" % (alert.__class__.__name__)
             method = getattr(alert_handler, m_name, None)
             if not method:
-                logger.debug("No method for %s", m_name)
+                #logger.debug("No method for %s", m_name)
                 continue
             try:
-                method(alert)
+                method(self.session, alert)
             except Exception:
                 logger.exception("Error calling handler")
+
+    def loop(self):
+        self._loop_thread.start()
+
+    def __len__(self):
+        return len(self.session.get_torrents())
